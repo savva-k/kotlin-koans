3580a87babf759c8bc5027245a672e3c9ffb5f3b
Savva Kodeikin <savva_kodeikin@epam.com>
2019-03-26 11:31:36 +0300

    Operators overloading

--- Conventions/Comparison/src/Task.kt
+++ Conventions/Comparison/src/Task.kt
@@ -1,5 +1,9 @@
 data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
-    /* TODO */
+    override fun compareTo(obj: MyDate): Int = when {
+        year != obj.year -> year - obj.year
+        month != obj.month -> month - obj.month
+        else -> dayOfMonth - obj.dayOfMonth
+    }
 }
 
 fun compare(date1: MyDate, date2: MyDate) = date1 < date2
