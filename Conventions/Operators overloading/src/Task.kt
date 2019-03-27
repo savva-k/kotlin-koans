87b126a78e8a81b9aa26cefffb80d0d28ce8e0b0
Savva Kodeikin <savva.kodeikin@gmail.com>
2019-03-27 21:01:31 +0300

    Operators overloading task is done

--- Conventions/Operators overloading/src/Task.kt
+++ Conventions/Operators overloading/src/Task.kt
@@ -4,12 +4,21 @@ data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
 
 enum class TimeInterval { DAY, WEEK, YEAR }
 
-operator fun MyDate.plus(timeInterval: TimeInterval): MyDate = TODO()
+data class TimeIntervalWithMultiplier(val timeInterval: TimeInterval, val multiplier: Int)
+
+operator fun TimeInterval.times(multiplier: Int): TimeIntervalWithMultiplier = TimeIntervalWithMultiplier(this, multiplier)
+
+operator fun MyDate.plus(timeInterval: TimeInterval): MyDate = addTimeIntervals(timeInterval, 1)
+
+operator fun MyDate.plus(timeIntervalWithMultiplier: TimeIntervalWithMultiplier): MyDate {
+    val (interval, multiplier) = timeIntervalWithMultiplier
+    return addTimeIntervals(interval, multiplier)
+}
 
 fun task1(today: MyDate): MyDate {
     return today + YEAR + WEEK
 }
 
 fun task2(today: MyDate): MyDate {
-    TODO("Uncomment") //return today + YEAR * 2 + WEEK * 3 + DAY * 5
+    return today + YEAR * 2 + WEEK * 3 + DAY * 5
 }
